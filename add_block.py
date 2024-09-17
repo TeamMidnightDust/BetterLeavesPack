bc809a6f629bce394532052ef7a912dfdc7d140b
Martin Prokoph <motschen@midnightdust.eu>
2024-09-17 19:47:11 +0200

    Add item model generation

--- add_block.py
+++ add_block.py
@@ -39,21 +39,34 @@ def generate(mod_namespace, block_name, notint):
     # Create the eight individual leaf models
     for i in range(1, 9):
         # Create structure for block model file
-        block_file = f"assets/{mod_namespace}/models/block/{block_name}{i}.json"
-        block_data = {
+        block_model_file = f"assets/{mod_namespace}/models/block/{block_name}{i}.json"
+        block_model_data = {
             "parent": f"block/{base_model}{i}",
             "textures": {
                 "0": f"{mod_namespace}:block/{block_name}"
             }
         }
-        with open(block_file, "w") as f:
-            json.dump(block_data, f, indent=4)
+        with open(block_model_file, "w") as f:
+            json.dump(block_model_data, f, indent=4)
+
+def generateItemModel(mod_namespace, block_name):
+    # Create models folder if it doesn't exist already
+    if not os.path.exists("assets/{}/models/item/".format(mod_namespace)):
+        os.makedirs("assets/{}/models/item/".format(mod_namespace))
+
+    item_model_file = f"assets/{mod_namespace}/models/item/{block_name}.json"
+    item_model_data = {
+        "parent": f"{mod_namespace}:block/{block_name}1"
+    }
+    with open(item_model_file, "w") as f:
+        json.dump(item_model_data, f, indent=4)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
                     description='This script can automatically generate blockstate and block model files for the Better Leaves resourcepack.',
                     epilog='Feel free to ask for help at http://discord.midnightdust.eu/')
 
+    parser.add_argument('--item', '-i', action='store_true')
     parser.add_argument('--notint', '-n', action='store_true')
     parser.add_argument('namespace', type=str) # Take namespace from the first argument
     parser.add_argument('block_names', nargs='+') # Take block names from the following arguments
@@ -63,5 +76,9 @@ if __name__ == '__main__':
     print()
 
     for block_name in args.block_names:
-        print(f"Generating blockstate and block model for {args.namespace}:{block_name}")
-        generate(args.namespace, block_name, args.notint)
+        if not args.item:
+            print(f"Generating blockstate and block model for {args.namespace}:{block_name}")
+            generate(args.namespace, block_name, args.notint)
+        else:
+            print(f"Generating item model for {args.namespace}:{block_name}")
+            generateItemModel(args.namespace, block_name)
